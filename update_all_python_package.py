b97585b068bf8199d947aaa925169a17efafec61
Frederik Andersen <82170487+AndersenFred@users.noreply.github.com>
2022-07-27 17:42:22 +0200

    Update and rename update_all_python_packyges.py to update_all_python_package.py

--- update_all_python_package.py
+++ update_all_python_package.py
@@ -6,3 +6,7 @@ except AttributeError:
   import pkg_resources
   packages = [dist.project_name for dist in pkg_resources.working_set]
 call("pip install --upgrade " + ' '.join(packages), shell=True)
+
+#Additionally on Windows one may add a update.bat file in autostart to update automatically
+#pyhton -m pip install --upgrade pip
+#python update_all_python_package.py
